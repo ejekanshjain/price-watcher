047b3434bd223ebab9a46f5d22921cdb130a6156
Ekansh Jain <ejekanshjain@gmail.com>
2022-03-16 23:33:27 +0530

    add on demand scrap function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -53,3 +53,54 @@ export const scheduledFunction = functions
         .join('\n\n\n')}`
     })
   })
+
+export const onDemandFunction = functions
+  .region('asia-south1')
+  .https.onRequest(async (req, res) => {
+    if (req.query.apiKey !== process.env.API_KEY) {
+      res.status(401).send('Invalid api key')
+      return
+    }
+    const timestamp = new Date().toISOString()
+    const db = admin.firestore()
+    const products = await db.collection('products').get()
+    const finalData: any[] = []
+    products.forEach(product => {
+      finalData.push({
+        id: product.id,
+        ...product.data()
+      })
+    })
+    if (!finalData.length) return
+    await Promise.all(
+      finalData.map(async fd => {
+        try {
+          const { data } = await axios.get(fd.url)
+          const $ = cheerio.load(data)
+          $(fd.priceSelector).each((i, el) => {
+            if (i > 0) return
+            fd.price = $(el).text().trim()
+          })
+        } catch (err) {
+          fd.error = true
+        }
+      })
+    )
+    const twilio = Twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    )
+    await twilio.messages.create({
+      from: process.env.TWILIO_NUMBER,
+      to: process.env.NOTIFY_NUMBER!,
+      body: `Price Watcher Results ${timestamp}\n\n\n\n${finalData
+        .map(
+          fd =>
+            `Name: ${fd.name}\n\nURL: ${fd.url}\n\nError: ${
+              fd.error ? 'Yes' : 'No'
+            }\n\nPrice: ${fd.price}\n\nPrice Selector: ${fd.priceSelector}`
+        )
+        .join('\n\n\n')}`
+    })
+    res.send('Done')
+  })
